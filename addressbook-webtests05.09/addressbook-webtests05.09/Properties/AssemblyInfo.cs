df869f882c67b8470d635cfab8f95345eb967482
Leonidushka <47792923+Leonidushka@users.noreply.github.com>
2020-09-05 17:53:57 +0300

    project created

---
+++ addressbook-webtests05.09/addressbook-webtests05.09/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook-webtests05.09")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Russia")]
+[assembly: AssemblyProduct("addressbook-webtests05.09")]
+[assembly: AssemblyCopyright("Copyright © Russia 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("120faa4c-583e-4d1c-b34f-c3a272e65bd2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
