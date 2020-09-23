88ab3166ad2d308f8d01b1df99427dc39b4d2d10
Om Leonid <doncorleone2005@mail.ru>
2020-09-23 22:48:29 +0300

    23.09: - создан Базовый класс для Тестов - почищены дубляжи в классах GroupCreationTests, ContactCreationTests, GroupRemovalTests - все тесты рабочие и запускаются без ошибок

---
+++ addressbook-webtests05.09/addressbook-webtests05.09/TestBase.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+
+namespace WebAddressbookTests
+{
+   public class TestBase
+    {
+        protected IWebDriver driver;
+        private StringBuilder verificationErrors;
+        protected string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://localhost/addressbook";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        protected void OpenHomePage()
+        {
+
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
+        protected void Login(AccountData account)
+        {
+
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.Id("LoginForm")).Click();
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+        protected void GoToGroupsPage()
+        {
+
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+        protected void InitGroupCreation()
+        {
+
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        protected void FillGroupForm(GroupData group)
+        {
+            //
+            driver.FindElement(By.Id("content")).Click();
+            driver.FindElement(By.Name("group_name")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_footer")).Click();
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+        }
+
+        protected void SubmitGroupCreation()
+        {
+
+            driver.FindElement(By.Name("submit")).Click();
+        }
+        protected void ReturnToGroupPage()
+        {
+
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+        protected void SelectGroup(int index)
+        {
+            driver.FindElement(By.XPath("(//input[@name='selected[]'])[" + index + "]")).Click();
+        }
+
+        protected void Remove()
+        {
+            driver.FindElement(By.Name("delete")).Click();
+        }
+
+        //contact creation
+
+
+        // Ни в какую тест не хочет добавлять через "Add New", пришлось сделать стартовой форму на создание контакта
+        protected void OpenHomePageForContacts()
+        {
+            driver.Navigate().GoToUrl("http://localhost/addressbook/edit.php");
+        }
+
+        protected void FillingNewContact(ContactData contact)
+        {
+            driver.FindElement(By.Name("firstname")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
+            driver.FindElement(By.Name("theform")).Click();
+        }
+
+        protected void SubmittingContactCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        protected void Logout()
+        {
+
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+
+    }
+}
+
